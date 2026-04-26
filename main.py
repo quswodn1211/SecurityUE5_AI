3d9ad1d89fd3efb45002ba621c55db3809979c97
quswodn1211 <quswodn1211@gmail.com>
2026-04-26 21:29:04 +0900

    tt

--- main.py
+++ main.py
@@ -16,42 +16,43 @@ class LogResult():
 # 디비와의 통신 GET
 app = FastAPI()
 
-response = requests.get(f"http://~~~~ 디비 주소/api/log/{log_id}")
+response = requests.get(f"https://perfectly-curdle-gecko.ngrok-free.dev/api/log/{1}")
+print(response)
 
-if response.status_code == 200:
-    data = response.json()
+# if response.status_code == 200:
+#     data = response.json()
 
-    player_id = data["player_id"]
-    sequence = data["sequence"]
+#     player_id = data["player_id"]
+#     sequence = data["sequence"]
 
-    print(player_id)
-    print(sequence)
-else:
-    print("로그 가져오기 실패:", response.status_code)
-
-
-# 데이터셋 생성
+#     print(player_id)
+#     print(sequence)
+# else:
+#     print("로그 가져오기 실패:", response.status_code)
 
 
-dataset = MovementDataset(
-    jsonl_path="movement_train.jsonl",
-    sequence_length=30,
-    feature_dim=12
-)
-
-train_loader = DataLoader(
-    dataset,
-    batch_size=64,
-    shuffle=True
-)
-
-model = train_model(train_loader, epochs=10)
+# # 데이터셋 생성
 
 
-# 결과를 저장하고 디비와의 통신 POST
-result = LogResult(player_id=player_id, abnormal_ratio=predict_anomaly(sequence))
+# dataset = MovementDataset(
+#     jsonl_path="movement_train.jsonl",
+#     sequence_length=30,
+#     feature_dim=12
+# )
 
-response = requests.post(
-    "http://~~~~~ 디비 주소/analysis_result",
-    json=result
-)
+# train_loader = DataLoader(
+#     dataset,
+#     batch_size=64,
+#     shuffle=True
+# )
+
+# model = train_model(train_loader, epochs=10)
+
+
+# # 결과를 저장하고 디비와의 통신 POST
+# result = LogResult(player_id=player_id, abnormal_ratio=predict_anomaly(sequence))
+
+# response = requests.post(
+#     "http://~~~~~ 디비 주소/analysis_result",
+#     json=result
+# )
